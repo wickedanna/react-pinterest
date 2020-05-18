496663e1277509f373ff4c62c83a04babb79a9c7
Kayla Melton <kannmelton2@gmail.com>
2020-05-18 18:33:03 -0500

    created component files

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+
+import Auth from '../components/Auth/Auth';
+import BoardContainer from '../components/BoardContainer/BoardContainer';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+
 import './App.scss';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">CRAP</button>
+        <h2>React Pinterest</h2>
+        <MyNavbar />
+        <Auth />
+        <BoardContainer />
       </div>
     );
   }
---
+++ src/components/BoardContainer/BoardContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './BoardContainer.scss';
+
+class BoardContainer extends React.Component {
+  render() {
+    return (
+      <div className="BoardContainer">
+        <h1>BOARD CONTAINER COMPONENT</h1>
+      </div>
+    );
+  }
+}
+
+export default BoardContainer;
