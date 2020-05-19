1955409e370ada0e3814b60f57fecef407175718
Kayla Melton <kannmelton2@gmail.com>
2020-05-18 19:12:11 -0500

    can log in into app

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,55 @@
 import React from 'react';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import fbConnection from '../helpers/data/connection';
+
+
 import Auth from '../components/Auth/Auth';
 import BoardContainer from '../components/BoardContainer/BoardContainer';
 import MyNavbar from '../components/MyNavbar/MyNavbar';
 
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      let componentToLoad = '';
+      if (authed) {
+        componentToLoad = <BoardContainer />;
+      } else {
+        componentToLoad = <Auth />;
+      }
+      return componentToLoad;
+    };
+
     return (
       <div className="App">
         <h2>React Pinterest</h2>
         <MyNavbar />
-        <Auth />
-        <BoardContainer />
+        {loadComponent()}
       </div>
     );
   }
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import './Auth.scss';
 
 class Auth extends React.Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
   render() {
     return (
       <div className="Auth">
-        <h1>AUTH COMPONENT</h1>
+        <button className="btn btn-info" onClick={this.loginClickEvent}>Google Auth</button>
       </div>
     );
   }
